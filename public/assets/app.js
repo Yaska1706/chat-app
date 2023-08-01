6751b5c4637cefccc7cb2a06c301def3c2519ba4
Yasin Kinyua Murithi <yasinkinyua@gmail.com>
2023-08-01 10:20:50 +0300

    Add styling and js functionality

--- public/assets/app.js
+++ public/assets/app.js
@@ -3,7 +3,9 @@ var app = new Vue({
     el:'#app',
     data: {
         ws: null,
-        serveUrl: "ws://127.0.0.1:8080/ws"
+        serveUrl: "ws://127.0.0.1:8080/ws",
+        messages: [],
+        newMessage: "",
     },
     mounted: function() {
         this.connectToWebsocket()
@@ -12,9 +14,24 @@ var app = new Vue({
         connectToWebsocket() {
             this.ws = new WebSocket(this.serveUrl);
             this.ws.addEventListener('open', (event) => {this.onWebSocketOpen(event)});
+            this.ws.addEventListener('message',(event) => {this.handleNewMessage(event)})
         },
         onWebSocketOpen(){
             console.log("connected to Room")
+        },
+        handleNewMessage(event) {
+            let data = event.data;
+            data = data.Split(/\r?\n/);
+            for (let i = 0; i < data; i++) {
+                let msg = JSON.parse(data[i]);
+                this.messages.push(msg)
+            }
+        },
+        sendMessage() {
+            if(this.newMessage !== "") {
+                this.ws.send(JSON.stringify({message:this.newMessage}));
+                this.newMessage = ""
+            }
         }
     }
 
